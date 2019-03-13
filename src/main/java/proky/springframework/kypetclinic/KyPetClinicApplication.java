1c17c4124cd753dad0d8c7d64c709aede87f1222
proky <klyuyko.yu@gmail.com>
2019-03-14 01:29:49 +0200

    init commit

---
+++ src/main/java/proky/springframework/kypetclinic/KyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package proky.springframework.kypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyPetClinicApplication.class, args);
+    }
+
+}
