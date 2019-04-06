e1e424d401e8ec34004ff2e0d90ad1dbe3f71f55
proky <klyuyko.yu@gmail.com>
2019-04-07 02:32:44 +0300

    Refactor service Interface to common Base interface. Closes #11

--- pet-clinic-data/src/main/java/proky/springframework/kypetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/proky/springframework/kypetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package proky.springframework.kypetclinic.services;
 
 import proky.springframework.kypetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService <Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
