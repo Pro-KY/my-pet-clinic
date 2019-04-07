2fafe3735fd2c99b818b30d0487a6a513233469d
proky <klyuyko.yu@gmail.com>
2019-04-07 17:29:45 +0300

    Impl Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/proky/springframework/kypetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package proky.springframework.kypetclinic.services.map;
+
+import proky.springframework.kypetclinic.model.Owner;
+import proky.springframework.kypetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
